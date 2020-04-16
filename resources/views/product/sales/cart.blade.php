da619b8ed1ada740a984b0d3c07b16bc4fc3519f
adith-widhiantara <172410101108@cs.unej.ac.id>
2020-04-16 17:39:55 +0700

    UI 24
    Revisi Sales Cart

--- resources/views/product/sales/cart.blade.php
+++ resources/views/product/sales/cart.blade.php
@@ -1,117 +1,87 @@
 @extends('base.materialize')
 @section('title', 'Keranjang | Annas Florist')
 
-@section('content')
-<head>
-  <link rel="stylesheet" href="{{ asset('css/cart.css') }}">
-  <script type="text/javascript" src="{{ asset('js/cart.js') }}"></script>
-</head>
+@section('head')
+  <link rel="stylesheet" href="{{ asset('css/sales/cart.css') }}">
+  <script src="{{ asset('js/sales/cart.js') }}"></script>
+@endsection
 
+@section('content')
 <!-- Navbar -->
-@include('base.navbar')
 <!-- End Navbar -->
 
-<!-- Spacing -->
-<div class="spacing"></div>
-<!-- End Spacing -->
-
-<!-- Cart -->
-<div class="cart-items">
-  <div class="cart">
-    <div class="cartt">
-      <div class="row cart-row">
-        <div class="col s1 m1 l1 xl1 cart2 height valign-wrapper hapusitem">
-          <a href="#delete2" class="btnn modal-trigger"><i class="material-icons">close</i></a>
-        </div>
-        <div class="col s5 offset-s1 m3 l3 xl3 height valign-wrapper cart2">
-          <img class="responsive-img hoverable" src="{{ asset('img/sales/sukulen panda.jpg') }}" alt="">
-        </div>
-        <div class="col s5 m3 l3 xl3 valign-wrapper cart2 judul">
-          <p>Sukulen Panda 2</p>
-        </div>
-        <div class="col s6 m3 l3 xl3 cart3 harga">
-          <p>Harga</p>
-          <span class="cart-price">Rp.10000</span>
-        </div>
-        <div class="col s5 m2 l2 xl2 cart3 harga">
-          <p>Jumlah Satuan</p>
-          <input class="cart-quantity-input" type="number" value="2" max="4" min="0" />
-        </div>
-
-        <!-- Modal Delete -->
-          <div id="delete2" class="modal">
-            <div class="modal-content">
-              <h4>Anda yakin menghapus produk ini?</h4>
-              <p>Sukulen Panda 2 | 2 item</p>
-            </div>
-            <div class="modal-footer">
-              <a href="#!" class="modal-close waves-effect waves-green btn-flat">Iya</a>
-              <a href="#!" class="modal-close waves-effect waves-green btn-flat tidak">Tidak</a>
-            </div>
-          </div>
-        <!-- End Modal Delete -->
-      </div>
-
-      <div class="row cart-row">
-        <div class="col s1 m1 l1 xl1 cart2 height valign-wrapper hapusitem">
-          <a href="#delete5" class="btnn modal-trigger"><i class="material-icons">close</i></a>
-        </div>
-        <div class="col s5 offset-s1 m3 l3 xl3 height valign-wrapper cart2">
-          <img class="responsive-img hoverable" src="{{ asset('img/sales/sukulen panda.jpg') }}" alt="">
-        </div>
-        <div class="col s5 m3 l3 xl3 valign-wrapper cart2 judul">
-          <p>Sukulen Panda</p>
-        </div>
-        <div class="col s6 m3 l3 xl3 cart3 harga">
-          <p>Harga</p>
-          <span class="cart-price">Rp.40000</span>
-        </div>
-        <div class="col s5 m2 l2 xl2 cart3 harga">
-          <p>Jumlah Satuan</p>
-          <input class="cart-quantity-input" type="number" value="2" max="4" min="0" />
-        </div>
-
-        <!-- Modal Delete -->
-          <div id="delete5" class="modal">
-            <div class="modal-content">
-              <h4>Anda yakin menghapus produk ini?</h4>
-              <p>Sukulen Panda | 2 item</p>
-            </div>
-            <div class="modal-footer">
-              <a href="#!" class="modal-close waves-effect waves-green btn-flat">Iya</a>
-              <a href="#!" class="modal-close waves-effect waves-green btn-flat tidak">Tidak</a>
-            </div>
-          </div>
-        <!-- End Modal Delete -->
-      </div>
-
-    </div>
-  </div>
+<!-- title -->
+<div class="container title">
+  <p>keranjang anda</p>
+  <p>anda mempunyai <span id="showcart"></span> barang di keranjang</p>
 </div>
-<!-- End Cart -->
+<!-- end title -->
 
-<!-- Total -->
-<section class="total">
-  <div class="totall">
-    <div class="row">
-      <div class="col m4 offset-m3 l3 offset-l4 xl3 offset-xl5 belisekarang2">
-        <p>Total Subharga Produk</p>
+<!-- cart -->
+<div class="container cart">
+  <!-- detail -->
+  <div class="row">
+    <div id="detail" class="col s12">
+      <div id="row" class="row bgwhite z-depth-2">
+        <div class="col xl2">
+          <img class="responsive-img hoverable" src="{{ asset('img/rental/4.png') }}" alt="">
+        </div>
+        <div class="col xl4 word1">
+          <p>sukulen panda<br><span>popular house plants</span></p>
+        </div>
+        <div class="col xl3">
+          <div id="12" class="input-field">
+            <input type="number" id="product1" class="formnumbers" name="ProductOne" onChange="changeTotalFromCount(this);" onLoad="changeTotalFromCount(this);" min="1" data-unitprice="10000" value="2" />
+            <label for="product1">Jumlah Barang</label>
+          </div>
+        </div>
+        <div class="col xl3">
+          <p></p>
+          <a href="#"><p class="exp">lihat selengkapnya</p></a>
+          <a href="#"><p class="rmv">hapus</p></a>
+        </div>
       </div>
 
-      <span class="col m2 l2 xl2 cart-total-price"></span>
-
-      <div class="col m2 offset-m1 l2 offset-l1 xl2 belisekarang">
-        <a href="#" role="button" type="submit">Beli Sekarang</a>
+      <div id="row" class="row bgwhite z-depth-2">
+        <div class="col xl2">
+          <img class="responsive-img hoverable" src="{{ asset('img/rental/4.png') }}" alt="">
+        </div>
+        <div class="col xl4 word1">
+          <p>sukulen panda<br><span>popular house plants</span></p>
+        </div>
+        <div class="col xl3">
+          <div class="input-field">
+            <input type="number" id="product2" class="formnumbers" name="ProductTwo" onChange="changeTotalFromCount(this);" onLoad="changeTotalFromCount(this);" min="1" data-unitprice="20000" value="2" />
+            <label for="product2">Jumlah Barang</label>
+          </div>
+        </div>
+        <div class="col xl3">
+          <p></p>
+          <a href="#"><p class="exp">lihat selengkapnya</p></a>
+          <a href="#"><p class="rmv">hapus</p></a>
+        </div>
       </div>
-
     </div>
   </div>
-</section>
-<!-- End Total -->
+  <!-- end detail -->
 
-<!-- Catalog -->
-@include('base.catalogsales')
-<!-- End Catalog -->
+  <!-- totalprice -->
+  <div class="row totalprice">
+    <div class="col xl4 offset-xl8">
+      <div class="row">
+        <div class="col xl6">
+          <p>Order Total</p>
+          <p><span id="showcart2"></span> Barang</p>
+        </div>
+        <div class="col xl6">
+          <p><span id="totalPriceDisplay"></span></p>
+        </div>
+      </div>
+      <a href="#">Checkout</a>
+    </div>
+  </div>
+  <!-- end totalprice -->
+</div>
+<!-- end cart -->
 
-@include('base.footer')
 @endsection
